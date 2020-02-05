1dddfb66cbbbbe4ff77b7e96ab0db5b68464b4ce
Robin Westling <Zynyx@CND8261K83.learnet.se>
2020-02-05 23:16:47 +0100

    Cleanup

--- Program.cs
+++ Program.cs
@@ -1,30 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using MySql.Data.MySqlClient;
-
-// Läs om sql injections
 
 public class Program
 {
 	Commands commands = new Commands();
-
 	static void Main()
     {
         Program p = new Program();
         p.Run();
     }
-
     void Run()
     {
 		commands.Connect();
-
-		
-		commands.SaveReciepts();
-		commands.SaveRefunds();
-		commands.LoadReciepts();
-		commands.LoadRefunds();
-		
         while (true)
         {
             string input = Console.ReadLine().ToLower();
@@ -34,36 +22,19 @@ public class Program
             switch (command[0])
             {
                 case "exit":
-					commands.SaveReciepts();
-					commands.SaveRefunds();
 					Environment.Exit(0);
                     break;
                 case "purchase":
                     commands.Purchase(command);
                     break;
-                case "find":
-                    commands.Find(command);
-                    break;
-                case "print":
-                    commands.Print();
-                    break;
-                case "refund":
-                    commands.Refund(command);
-                    break;
-                case "amount":
-                    commands.AmountSold();
-                    break;
 				case "load":
-					commands.LoadReciepts();
+					commands.Load();
 					break;
-				case "l":
-					commands.LoadDB();
+				case "find":
+					commands.Find(command);
 					break;
-				case "f":
-					commands.FindDB(command);
-					break;
-				case "r":
-					commands.RefundDB(command);
+				case "refund":
+					commands.Refund(command);
 					break;
 				case "sum":
 					commands.Sum();
@@ -78,11 +49,7 @@ public class Program
 class Commands
 {
 	MySqlConnection con;
-	string Recieptpath = @"C:..\..\..\Data\reciepts.txt";
-	string Refundtpath = @"C:..\..\..\Data\refunds.txt";
 	SystemCommands scomm = new SystemCommands();
-    List<Reciept> reciepts = new List<Reciept>();
-    List<Reciept> refunds = new List<Reciept>(); 
     public void Purchase(string[] purchase)
     {
         if (purchase.Length != 5)
@@ -110,217 +77,12 @@ class Commands
                 Console.WriteLine("Succesfully purchased");
                 Console.ResetColor();
             }
-            catch (RecieptAlreadyExistException)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("A reciept with that name already exists");
-                Console.ResetColor();
-            }
-            catch (Exception)
-            {
-                scomm.WrongInput();
-            }
-        }
-    }
-    public void Refund(string[] refund)
-    {
-        if (refund.Length > 2)
-        {
-            scomm.SyntaxError();
-        }
-        else
-        {
-            try
-            {
-                for (int i = reciepts.Count - 1; i >= 0; i--)
-                {
-                    if (reciepts[i].GetName().ToLower() == refund[1].ToLower())
-                    {
-						//refunds.AddRange(reciept.Select(reciept[i] => i));
-						// Reciept(reciepts[i].GetName(), reciepts[i].GetAAmount(), reciepts[i].GetCAmount(), reciepts[i].GetSAmount())
-
-						Reciept tRefund = reciepts[i];
-                        refunds.Add(tRefund);
-                        reciepts.RemoveAt(i);
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("Succesfully refunded");
-                        Console.ResetColor();
-                    }
-                }
-            }
             catch
             {
                 scomm.WrongInput();
             }
         }
     }
-    public void Add(string name, string aTickets, string cTickets, string sTickets)
-    {
-        Reciept reciept = new Reciept(name, int.Parse(aTickets), int.Parse(cTickets), int.Parse(sTickets));
-        scomm.CheckDuplicate(reciept, reciepts, refunds);
-        reciepts.Add(reciept);
-		AddDB(name, aTickets, cTickets, sTickets);
-    }
-    public void Print()
-    {
-        bool found = false;
-        if (reciepts.Count != 0)
-        {
-            Console.WriteLine("Purchased:");
-            
-            for (int i = 0; i < reciepts.Count; i++)
-            {
-                if (reciepts[i] != null)
-                {
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine(reciepts[i].GetName());
-                    Console.ResetColor();
-                    found = true;
-                }
-            }
-        }
-        if (refunds.Count != 0)
-        {
-            Console.WriteLine("Refunded:");
-            for (int i = 0; i < refunds.Count; i++)
-            {
-                if (refunds[i] != null)
-                {
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine(refunds[i].GetName());
-                    Console.ResetColor();
-                    found = true;
-                }
-            }
-        }
-        if(!found)
-        {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("There are no purchases or refunds");
-            Console.ResetColor();
-        }
-    }
-    public void Find(string[] search)
-    {
-        if (search.Length > 2)
-        {
-            scomm.SyntaxError();
-        }
-        else
-        {
-            try
-            {
-                bool found = false;
-                for (int i = reciepts.Count - 1; i >= 0; i--)
-                {
-                    if (reciepts[i].GetName().ToLower() == search[1].ToLower())
-                    {
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.WriteLine(reciepts[i].GetReciept());
-                        Console.ResetColor();
-                        found = true;
-                    }
-                }
-                for (int i = refunds.Count - 1; i >= 0; i--)
-                {
-                    if (refunds[i].GetName().ToLower() == search[1].ToLower())
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("!Refunded!");
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.WriteLine(refunds[i].GetReciept());
-                        Console.ResetColor();
-                        found = true;
-                    }
-                }
-                if (!found)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("There is no reciept with that name");
-                    Console.ResetColor();
-                }
-            }
-            catch
-            {
-                scomm.WrongInput();
-            }
-        }
-    }
-    public void AmountSold()
-    {
-		int adultTickets = 0;
-		int childTickets = 0;
-		int seniorTickets = 0;
-       foreach(Reciept r in reciepts)
-		{
-			adultTickets += r.GetAAmount();
-			childTickets += r.GetCAmount();
-			seniorTickets += r.GetSAmount(); 
-		}
-		Console.WriteLine($"Total Tickets Sold: {adultTickets + childTickets + seniorTickets}");
-		Console.WriteLine($"Adult Tickets Sold: {adultTickets}");
-		Console.WriteLine($"Child Tickets Sold: {childTickets}");
-		Console.WriteLine($"Senior Tickets Sold: {seniorTickets}");
-    }
-	public void AmountRefunded()
-    {
-		int adultTickets = 0;
-		int childTickets = 0;
-		int seniorTickets = 0;
-       foreach(Reciept r in refunds)
-		{
-			adultTickets += r.GetAAmount();
-			childTickets += r.GetCAmount();
-			seniorTickets += r.GetSAmount(); 
-		}
-		Console.WriteLine($"Total Tickets Refunded: {adultTickets + childTickets + seniorTickets}");
-		Console.WriteLine($"Adult Tickets Refunded: {adultTickets}");
-		Console.WriteLine($"Child Tickets Refunded: {childTickets}");
-		Console.WriteLine($"Senior Tickets Refunded: {seniorTickets}");
-    }
-	public void SaveReciepts()
-	{
-		StreamWriter Recieptsw = new StreamWriter(Recieptpath, true);
-		foreach (Reciept r in reciepts)
-		{
-			Recieptsw.WriteLine(r.Info()); ; 
-		}
-		Recieptsw.Close();
-	}
-	public void SaveRefunds()
-	{
-		StreamWriter Refundsw = new StreamWriter(Refundtpath, true);
-		foreach (Reciept r in refunds)
-		{
-			Refundsw.WriteLine(r.Info()); ;
-		}
-		Refundsw.Close();
-	}
-	public void LoadReciepts()
-	{
-		StreamReader Recietsr = new StreamReader(Recieptpath);
-		string text;
-        while ((text = Recietsr.ReadLine()) != null)
-        {
-            string[] strings = text.Split(char.Parse(","));
-			Reciept reciept = new Reciept(strings[0], int.Parse(strings[1]), int.Parse(strings[2]), int.Parse(strings[3]));
-			reciepts.Add(reciept);
-		}
-        Recietsr.Close();
-	}
-	public void LoadRefunds()
-	{
-		StreamReader Refundsr = new StreamReader(Refundtpath);
-		string text;
-		while ((text = Refundsr.ReadLine()) != null)
-		{
-			string[] strings = text.Split(char.Parse(","));
-			Reciept refund = new Reciept(strings[0], int.Parse(strings[1]), int.Parse(strings[2]), int.Parse(strings[3]));
-			refunds.Add(refund);
-		}
-		Refundsr.Close();
-	}
-
 	public void Connect()
 	{
 		string cs = @"server=localhost;userid=root;password=Robin789;database=ticketsales";
@@ -328,14 +90,14 @@ class Commands
 		con = new MySqlConnection(cs);
 		con.Open();
 	}
-	public void AddDB(string name, string aTickets, string cTickets, string sTickets)
+	public void Add(string name, string aTickets, string cTickets, string sTickets)
 	{
 		var stm = $"insert into purchases (name,aTickets,cTickets,sTickets) values ('{name}',{aTickets},{cTickets},{sTickets});";
 		var cmd = new MySqlCommand(stm, con);
 
 		cmd.ExecuteScalar();
 	}
-	public void RefundDB(string[] refund)
+	public void Refund(string[] refund)
 	{
 		var stm = $"UPDATE `ticketsales`.`purchases` SET `refunded` = 1 WHERE(`NAME` = '{refund[1]}');";
 		Console.WriteLine(stm);
@@ -343,7 +105,7 @@ class Commands
 
 		cmd.ExecuteScalar();
 	}
-	public void LoadDB()
+	public void Load()
 	{
 		string sql = "SELECT * FROM purchases;";
 		var cmd = new MySqlCommand(sql, con);
@@ -379,7 +141,7 @@ class Commands
 			Console.WriteLine("Adult Tickets: {0}, Child Tickets: {1}, Senior Tickets {2}", rdr.GetInt32(1), rdr.GetInt32(2), rdr.GetInt32(3));
 		}
 	}
-	public void FindDB(string[] search)
+	public void Find(string[] search)
 	{
 		if (search.Length > 2)
 		{
@@ -414,9 +176,6 @@ class Commands
 		}
 	}
 }
-
-class RecieptAlreadyExistException : Exception { }
-
 class SystemCommands
 {
 	public void SyntaxError()
@@ -431,80 +190,5 @@ class SystemCommands
         Console.WriteLine("Wrong input, try again");
         Console.ResetColor();
     }
-	public void CheckDuplicate(Reciept reciept, List<Reciept> reciepts, List<Reciept> refunds)
-	{
-		for (int i = 0; i < reciepts.Count | i < refunds.Count; i++)
-		{
-			if (reciepts[i] != null)
-			{
-				if (reciepts[i].GetName().ToLower() == reciept.GetName().ToLower())
-				{
-					throw new RecieptAlreadyExistException();
-				}
-			}
-		}
-	}
 }
 
-class Reciept
-{
-    private string name;
-    private int aTickets;
-    private int cTickets;
-    private int sTickets;
-
-    private int aPrice = 100;
-    private int cPrice = 25;
-    private int sPrice = 75;
-
-    private int tPrice;
-    private string returnvalue;
-    public Reciept(string name, int aTickets, int cTickets, int sTickets)
-    {
-        this.name = name;
-        this.aTickets = aTickets;
-        this.cTickets = cTickets;
-        this.sTickets = sTickets;
-    }
-	public string Info()
-	{
-		return $"{name},{aTickets},{cTickets},{sTickets}";
-	}
-    public int GetAAmount()
-    {
-        return aTickets;
-    }
-    public int GetCAmount()
-    {
-        return cTickets;
-    }
-    public int GetSAmount()
-    {
-        return sTickets;
-    }
-    public string GetName()
-    {
-        return name;
-    }
-    public string GetReciept()
-    {
-        returnvalue = $"Name: {name} \n";
-        if (aTickets > 0)
-        {
-            returnvalue = returnvalue + $"Adult: {aTickets} \n"; //string interpolation
-            tPrice = tPrice + aTickets * aPrice;
-        }
-        if (cTickets > 0)
-        {
-            returnvalue = returnvalue + $"Child: {cTickets} \n";
-            tPrice = tPrice + cTickets * cPrice;
-        }
-        if (sTickets > 0)
-        {
-            returnvalue = returnvalue + $"Senior: {sTickets} \n";
-            tPrice = tPrice + sTickets * sPrice;
-        }
-        returnvalue = returnvalue + $"\nTotal Price: {tPrice}kr";
-        return returnvalue;
-    }
-}
