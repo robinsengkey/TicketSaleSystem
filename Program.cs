d0b411515dec3337276e67449c5030bb2641e9c7
Robin Westling <Zynyx@CND8261K83.learnet.se>
2020-02-07 14:28:51 +0100

    final commit

--- Program.cs
+++ Program.cs
@@ -88,52 +88,65 @@ class Commands
 	}
 	public void Add(string name, string aTickets, string cTickets, string sTickets)
 	{
-		var stm = $"insert into purchases (name,aTickets,cTickets,sTickets) values ('{name}',{aTickets},{cTickets},{sTickets});";
-		var cmd = new MySqlCommand(stm, con);
+		var sql = $"insert into purchases (name,aTickets,cTickets,sTickets) values ('{name}',{aTickets},{cTickets},{sTickets});";
+		var cmd = new MySqlCommand(sql, con);
 		cmd.ExecuteScalar();
 	}
 	public void Refund(string[] refund)
 	{
-		var stm = $"UPDATE `ticketsales`.`purchases` SET `refunded` = 1 WHERE(`NAME` = '{refund[1]}');";
-		var cmd = new MySqlCommand(stm, con);
-		cmd.ExecuteScalar();
-		Console.WriteLine("Refunded");
+		try
+		{
+			var sql = $"UPDATE `ticketsales`.`purchases` SET `refunded` = 1 WHERE(`NAME` = '{refund[1]}');";
+			var cmd = new MySqlCommand(sql, con);
+			cmd.ExecuteScalar();
+			Console.WriteLine("Refunded");
+		}
+		catch { }
 	}
 	public void Load()
 	{
-		string sql = "SELECT * FROM purchases;";
-		var cmd = new MySqlCommand(sql, con);
-		MySqlDataReader rdr = cmd.ExecuteReader();
-		while (rdr.Read())
+		try
 		{
-			Console.Write("ID: {0}, Name: {1}, Adult Tickets: {2}, Child Tickets: {3}, Senior Tickets {4}", rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2), rdr.GetInt32(3), rdr.GetInt32(4));
-			if (rdr.GetInt32(5) == 1)
+			string sql = "SELECT * FROM purchases;";
+			var cmd = new MySqlCommand(sql, con);
+			MySqlDataReader rdr = cmd.ExecuteReader();
+			while (rdr.Read())
 			{
-				Console.Write(", !REFUNDED!");
-			}
-			Console.WriteLine("");	
+				Console.Write("ID: {0}, Name: {1}, Adult Tickets: {2}, Child Tickets: {3}, Senior Tickets {4}", rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2), rdr.GetInt32(3), rdr.GetInt32(4));
+				if (rdr.GetInt32(5) == 1)
+				{
+					Console.Write(", !REFUNDED!");
+				}
+				Console.WriteLine("");
 
+			}
+			rdr.Close();
 		}
-		rdr.Close();
+		catch { }
 	}
 	public void Sum()
 	{
-		string sql = "select refunded, sum(aTickets), sum(cTickets), sum(sTickets) from purchases group by 1;";
-		var cmd = new MySqlCommand(sql, con);
-		MySqlDataReader rdr = cmd.ExecuteReader();
-		while (rdr.Read())
+		try
 		{
-			var refunded = rdr.GetBoolean(0);
-			if (refunded)
+			string sql = "select refunded, sum(aTickets), sum(cTickets), sum(sTickets) from purchases group by 1;";
+			var cmd = new MySqlCommand(sql, con);
+			MySqlDataReader rdr = cmd.ExecuteReader();
+			while (rdr.Read())
 			{
-				Console.WriteLine("refunded:");
-			} else
-			{
-				Console.WriteLine("purchased:");
+				var refunded = rdr.GetBoolean(0);
+				if (refunded)
+				{
+					Console.WriteLine("refunded:");
+				}
+				else
+				{
+					Console.WriteLine("purchased:");
+				}
+				Console.WriteLine("Adult Tickets: {0}, Child Tickets: {1}, Senior Tickets {2}, Total {3}", rdr.GetInt32(1), rdr.GetInt32(2), rdr.GetInt32(3), rdr.GetInt32(1) + rdr.GetInt32(2) + rdr.GetInt32(3));
 			}
-			Console.WriteLine("Adult Tickets: {0}, Child Tickets: {1}, Senior Tickets {2}, Total {3}", rdr.GetInt32(1), rdr.GetInt32(2), rdr.GetInt32(3), rdr.GetInt32(1+2+3));
+			rdr.Close();
 		}
-		rdr.Close();
+		catch { }
 	}
 	public void Find(string[] search)
 	{
